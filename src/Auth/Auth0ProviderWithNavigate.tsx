8194f4d9da25fb42f40f9237aaf9d1d6b4336627
galbarcessat <galbarcessat1@gmail.com>
2024-09-22 19:58:07 +0300

    AuthCallbackPage, MyUserApi ,MobileNavLinks

--- src/Auth/Auth0ProviderWithNavigate.tsx
+++ src/Auth/Auth0ProviderWithNavigate.tsx
@@ -1,10 +1,13 @@
+import { useCreateMyUser } from '@/api/MyUserApi';
 import { AppState, Auth0Provider, User } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
 
 type Props = {
     children: React.ReactNode
 }
 
 export function Auth0ProviderWithNavigate({ children }: Props) {
+    const navigate = useNavigate()
     const domain = import.meta.env.VITE_AUTH0_DOMAIN
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL
@@ -14,7 +17,7 @@ export function Auth0ProviderWithNavigate({ children }: Props) {
     }
 
     function onRedirectCallback(appState?: AppState, user?: User) {
-        console.log('user:', user)
+        navigate("/auth-callback")
     }
 
     return (
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './global.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { AppRoutes } from './AppRoutes'
-import { Auth0ProviderWithNavigate } from './Auth/Auth0ProviderWithNavigate'
+import { Auth0ProviderWithNavigate } from './auth/Auth0ProviderWithNavigate'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+
+    }
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
-      <Auth0ProviderWithNavigate>
-        <AppRoutes />
-      </Auth0ProviderWithNavigate>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNavigate>
+          <AppRoutes />
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </Router>
   </StrictMode>,
 )
